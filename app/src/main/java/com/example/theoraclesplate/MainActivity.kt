646491066a49cb93b8a3eb19fb6b324042a3be70
Aditya Kumar Mishra <vadityamishra777@gmail.com>
2025-06-12 09:19:06 +0530

    Merge remote-tracking branch 'origin/main'

--- app/src/main/java/com/example/theoraclesplate/MainActivity.kt
+++ app/src/main/java/com/example/theoraclesplate/MainActivity.kt
@@ -6,14 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-           var NavController: NavController=findViewById(R.id.nav_host_fragment_content_main)
+           var NavController:NavController = findNavController(R.id.fragmentContainerView)
         }
     }
-}
