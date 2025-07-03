09d2dc56782ade4d984df38c64926ef2c50c15df
Aditya Kumar Mishra <vadityamishra777@gmail.com>
2025-07-03 15:38:30 +0530

    chnages

--- app/src/main/java/com/example/theoraclesplate/MainActivity.kt
+++ app/src/main/java/com/example/theoraclesplate/MainActivity.kt
@@ -7,11 +7,23 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import com.example.theoraclesplate.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class  MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
            var NavController:NavController = findNavController(R.id.fragmentContainerView)
+           var bottomnav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+            bottomnav.setupWithNavController(NavController)
+        binding.notificationButton.setOnClickListener {
+            val bottomSheetDialog = Notification_Bottom_Fragment()
+            bottomSheetDialog.show(supportFragmentManager, "Test")
+        }
         }
     }
--- app/src/main/java/com/example/theoraclesplate/Notification_Bottom_Fragment.kt
+++ app/src/main/java/com/example/theoraclesplate/Notification_Bottom_Fragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.theoraclesplate.adapter.NotificationAdaptor
 import com.example.theoraclesplate.databinding.FragmentNotificationBottomBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
@@ -24,6 +26,14 @@ class Notification_Bottom_Fragment : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
 binding=FragmentNotificationBottomBinding.inflate(layoutInflater,container,false)
+        val notifications = listOf("Your order has been Canceled Successfully","Order has been taken by the driver","Cpngrats Your Order Placed")
+        val notificationImage = listOf(R.drawable.cancel,R.drawable.driver,R.drawable.congrats)
+        val adapter = NotificationAdaptor(
+            ArrayList(notifications),
+            ArrayList(notificationImage))
+        binding.notificationRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.notificationRecyclerView.adapter = adapter
+
         return binding.root
     }
 
