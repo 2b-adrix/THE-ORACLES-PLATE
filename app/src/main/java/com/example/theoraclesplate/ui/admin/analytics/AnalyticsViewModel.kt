afc3341a83e2c3c5ab76ded532e2aea529991998
THE-ORACLE'S-PLATE <vadityamishra777@gmail.com>
2026-02-16 00:40:11 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/theoraclesplate/SigninActivity.kt
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/layout/cart_item.xml

--- app/src/main/java/com/example/theoraclesplate/ui/admin/analytics/AnalyticsViewModel.kt
+++ app/src/main/java/com/example/theoraclesplate/ui/admin/analytics/AnalyticsViewModel.kt
@@ -1,13 +1,13 @@
 package com.example.theoraclesplate.ui.admin.analytics
 
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.theoraclesplate.domain.use_case.AdminUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,24 +15,40 @@ class AnalyticsViewModel @Inject constructor(
     private val adminUseCases: AdminUseCases
 ) : ViewModel() {
 
-    private val _state = mutableStateOf(AnalyticsState())
-    val state: State<AnalyticsState> = _state
+    private val _state = MutableStateFlow(AnalyticsState())
+    val state = _state.asStateFlow()
 
     init {
         getAnalyticsData()
     }
 
     private fun getAnalyticsData() {
-        adminUseCases.getAnalyticsData().onEach { data ->
-            _state.value = state.value.copy(
-                analyticsData = data.getOrNull() ?: emptyMap(), 
-                isLoading = false
-            )
-        }.launchIn(viewModelScope)
+        viewModelScope.launch {
+            adminUseCases.getAnalyticsData().collectLatest { result ->
+                _state.value = when {
+                    result.isSuccess -> {
+                        state.value.copy(
+                            analyticsData = result.getOrNull() ?: emptyMap(),
+                            isLoading = false
+                        )
+                    }
+                    result.isFailure -> {
+                        state.value.copy(
+                            error = result.exceptionOrNull()?.message,
+                            isLoading = false
+                        )
+                    }
+                    else -> {
+                        state.value.copy(isLoading = true)
+                    }
+                }
+            }
+        }
     }
 }
 
 data class AnalyticsState(
     val analyticsData: Map<String, Any> = emptyMap(),
-    val isLoading: Boolean = true
+    val isLoading: Boolean = false,
+    val error: String? = null
 )
